bf6c3df33deb45bd0fc06e876d0d67df489b7c66
Cade-Spaulding <51171344+Cade-Spaulding@users.noreply.github.com>
2019-06-14 15:02:46 -0500

    Update 8-bit battle.py

--- 8-bit battle.py
+++ 8-bit battle.py
@@ -493,8 +493,9 @@ while True:
                 if perkon[8]<250:
                     perkon[8]+=1
         if death==True:
+             location==[[550,'man','L',25,0,False,0,450,250,characters[0][7],characters[random.randint(0,11)],[0,[None]],0],[150,'AI','R',25,0,False,0,450,250,characters[0][7],characters[random.randint(0,11)],[0,None],0]]
             if not difficulty=='PvP':
-            location==[[550,'man','L',25,0,False,0,450,250,characters[0][7],characters[random.randint(0,11)],[0,[None]],0],[150,'AI','R',25,0,False,0,450,250,characters[0][7],characters[random.randint(0,11)],[0,None],0]]
+           
                 if dead=='AI':
                     text=basicFont.render('YOU WIN',True,green,white)
                     textRect=text.get_rect()
